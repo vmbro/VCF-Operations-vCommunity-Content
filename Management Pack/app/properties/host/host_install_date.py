2dd6793ddbd48c0458821957d1f025970e3764c8
Scott R Bowe <scottb@sentania.net>
2025-08-21 15:51:28 -0500

    add install date

---
+++ Management Pack/app/properties/host/host_install_date.py
@@ -0,0 +1,22 @@
+
+#  Copyright 2025 vCommunity Content MP
+#  Author: Scott Bowe scott.bowe@broadcom.com
+
+import logging
+from datetime import datetime, timezone   
+
+logger = logging.getLogger(__name__)
+
+def collect_host_software_properties(host_obj, host):
+    imageConfigManager = host.configManager.imageConfigManager
+    if imageConfigManager:
+        try:
+            install_dt = imageConfigManager.installDate()  # datetime in UTC (vim.DateTime)
+            if install_dt:
+                dt_utc = install_dt.astimezone(timezone.utc)
+                host_obj.with_property("Config|Install Date|UTC", dt_utc.isoformat())
+                host_obj.with_property("Config|Install Date|EpochSeconds", int(dt_utc.timestamp()))
+            else:
+                host_obj.with_property("Config|Install Date|UTC", "unknown")
+        except Exception as e:
+            logger.exception(f"Failed to retrieve install date for host '{host.name}' (MoID: {host._moId}): {e}")
